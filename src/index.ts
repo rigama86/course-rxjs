0121d56432c4d7edd34fd28d76e56e5ea13f6f97
Ricardo Gallardo Gallardo <rgalllardo@novasolutionsystems.com>
2020-04-04 01:04:30 -0600

    create subjects

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { Observable, Observer, Subject } from 'rxjs';
+
+const observer: Observer<any> = {
+    next: value => console.log( 'next [next]: ', value),
+    error: error => console.warn( 'error [obs]: ', error),
+    complete: () => console.info( 'complete [obs]' )
+};
+
+const intervalo$ = new Observable<number>( subs => {
+    const intervalID = setInterval( 
+        () => subs.next( Math.random() ), 4000 
+    );
+    return () => clearInterval( intervalID );
+} );
+
+const subject$ = new Subject();
+intervalo$.subscribe(subject$);
+
+// const subs1 = intervalo$.subscribe( console.log );
+// const subs2 = intervalo$.subscribe( console.log );
+
+const subs1 = subject$.subscribe( rnd => console.log('subs1: ', rnd) );
+const subs2 = subject$.subscribe( rnd => console.log('subs2: ', rnd) );
